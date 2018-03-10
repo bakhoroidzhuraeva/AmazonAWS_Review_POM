2cd2592394418dbb9d103efcb30d3222a8201fd0
MarifatA <33642379+MarifatA@users.noreply.github.com>
2018-03-10 14:16:06 -0500

    Update TestOne.java

--- src/com/amazonAWS/tests/TestOne.java
+++ src/com/amazonAWS/tests/TestOne.java
@@ -3,8 +3,8 @@ package com.amazonAWS.tests;
 public class TestOne {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// TODO Auto-gBela enerated method stub
+Beka kandia
 	}
 
 }
